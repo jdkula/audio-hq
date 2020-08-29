e039838162f42de48671b3dbfa80080560eaa4f3
Jonathan Kula <jonathan@jdkula.dev>
2020-08-29 04:29:06 -0700

    Include size calculation on clear dialog!

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,8 +68,45 @@ const Logo = styled.div`
     user-select: none;
 `;
 
+// thanks https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string/10420404 !
+function humanFileSize(bytes: number, si = false, dp = 1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+    return bytes.toFixed(dp) + ' ' + units[u];
+}
+
 const ConfirmDeleteAllDialog: FC<DialogProps> = (props) => {
     const [deleting, setDeleting] = useState(false);
+
+    const [size, setSize] = useState<number | null>(null);
+
+    useEffect(() => {
+        if (props.open) {
+            new PouchDB('cache')
+                .allDocs({ attachments: true, binary: true, include_docs: true })
+                .then((docs) => docs.rows.flatMap((doc) => Object.values(doc.doc?._attachments ?? {})))
+                .then((attachments) =>
+                    attachments.map((attachment) => ((attachment as PouchDB.Core.FullAttachment).data as Blob).size),
+                )
+                .then((sizes) => sizes.reduce((sum, cur) => sum + cur, 0))
+                .then((totalSize) => setSize(totalSize));
+        }
+    }, [props.open]);
 
     const doDelete = () => {
         setDeleting(true);
@@ -80,8 +117,14 @@ const ConfirmDeleteAllDialog: FC<DialogProps> = (props) => {
         <Dialog {...props}>
             <DialogTitle>Clear Audio Cache</DialogTitle>
             <DialogContent dividers>
-                Are you sure? This will completely clear all stored music, meaning you&apos;ll have to download
-                everything from scratch!
+                <Typography variant="body1">
+                    Are you sure? This will completely clear all stored music, meaning you&apos;ll have to download
+                    everything from scratch!
+                </Typography>
+                <Box m="1rem" />
+                <Typography variant="button">
+                    Total Size: {size === null ? 'Calculating...' : humanFileSize(size, true)}
+                </Typography>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => props.onClose?.({}, 'escapeKeyDown')}>Cancel</Button>
