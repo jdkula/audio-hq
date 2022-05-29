b26f7a0123175aa4dc8c62390f5cabd2f205bdc5
Jonathan Kula <jonathan@jdkula.dev>
2022-05-29 03:40:00 -0700

    Remove API– will be replaced with workers

--- frontend/pages/api/[ws]/files.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-import { findOrCreateWorkspace } from './index';
-
-const WorkspaceFiles: NextApiHandler = async (req, res) => {
-    res.json((await findOrCreateWorkspace(req.query.ws as string)).files);
-};
-
-export default WorkspaceFiles;
--- frontend/pages/api/[ws]/jobs.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-import { findOrCreateWorkspace } from './index';
-
-const WorkspaceJobs: NextApiHandler = async (req, res) => {
-    res.json((await findOrCreateWorkspace(req.query.ws as string)).jobs);
-};
-
-export default WorkspaceJobs;
