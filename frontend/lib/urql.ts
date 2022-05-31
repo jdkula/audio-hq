487839516c64ff48b711460967a74bbd55edc1bb
Jonathan Kula <jonathan@jdkula.dev>
2022-05-31 02:04:33 -0700

    Update urql to use the graphcache, and auto-parse dates

--- frontend/lib/urql.ts
+++ frontend/lib/urql.ts
@@ -1,17 +1,71 @@
 import { createClient } from 'graphql-ws';
 import { useCallback, useEffect, useState } from 'react';
-import { Client, defaultExchanges, subscriptionExchange } from 'urql';
+import {
+    Client,
+    defaultExchanges,
+    subscriptionExchange,
+    errorExchange,
+    dedupExchange,
+    fetchExchange,
+    cacheExchange as documentCacheExchange,
+} from 'urql';
+import { devtoolsExchange } from '@urql/devtools';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import customScalarsExchange from 'urql-custom-scalars-exchange';
+import schema from '~/graphql.schema.json';
+import { Play_Status, PlayTrackMutation } from './generated/graphql';
+import { IntrospectionData } from '@urql/exchange-graphcache/dist/types/ast';
 
 function createUrqlClient(): Client {
+    if (typeof window === 'undefined') {
+        return new Client({ url: process.env.NEXT_PUBLIC_HASURA_URL_HTTP as string });
+    }
     const wsClient = createClient({
         url: process.env.NEXT_PUBLIC_HASURA_URL_WS as string,
         retryAttempts: 10,
     });
 
-    const client = new Client({
+    return new Client({
         url: process.env.NEXT_PUBLIC_HASURA_URL_HTTP as string,
         exchanges: [
-            ...defaultExchanges,
+            devtoolsExchange,
+            errorExchange({
+                onError: (error, operation) => {
+                    console.warn(error, operation);
+                },
+            }),
+            customScalarsExchange({ schema: schema as any, scalars: { timestamptz: (value) => new Date(value) } }),
+            dedupExchange,
+            // documentCacheExchange,
+            cacheExchange({
+                schema: schema as IntrospectionData,
+                updates: {
+                    Mutation: {
+                        insert_play_status_one(_result, args, cache, _info) {
+                            cache
+                                .inspectFields('query_root')
+                                .filter((field) => field.fieldName === 'play_status')
+                                .forEach((field) => {
+                                    console.log('Invalidating in mutation', field);
+                                    cache.invalidate('query_root', field.fieldKey);
+                                });
+                        },
+                    },
+                    Subscription: {
+                        event(_result, args, cache, _info) {
+                            const invalidator = ((_result.event as any)?.[0]?.invalidate as string) ?? undefined;
+                            cache
+                                .inspectFields('query_root')
+                                .filter((field) => field.fieldName === invalidator)
+                                .forEach((field) => {
+                                    console.log('Invalidating in subscription', field);
+                                    cache.invalidate('query_root', field.fieldKey);
+                                });
+                        },
+                    },
+                },
+            }),
+            fetchExchange,
             subscriptionExchange({
                 forwardSubscription: (operation) => ({
                     subscribe: (sink) => ({
@@ -21,8 +75,6 @@ function createUrqlClient(): Client {
             }),
         ],
     });
-
-    return client;
 }
 
 export function useUrqlClient(): { client: Client; reinitialize: () => void } {
