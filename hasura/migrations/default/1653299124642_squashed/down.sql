96c19ea42ce59e3599a00123af5fc814014846f0
Jonathan Kula <jonathan@jdkula.dev>
2022-05-31 02:03:22 -0700

    Database updates
    + Use proper workspace ids and names.
    + Use database triggers to manage ensuring there's only one main entry
    + Keep track of when play statuses are created
    + Create special event table for receiving events over subscriptions from Hasura (still WIP)

--- hasura/migrations/default/1653299124642_squashed/down.sql
+++ hasura/migrations/default/1653299124642_squashed/down.sql
@@ -1,3 +1,8 @@
+DROP TRIGGER job_update_event ON public.job;
+DROP TRIGGER file_update_event ON public.file;
+DROP TRIGGER status_update_event ON public.play_status;
+DROP FUNCTION public.create_event();
+DROP TABLE public.event;
 DROP TABLE public.delete_job;
 
 DROP TABLE public.job;
@@ -6,14 +11,15 @@ DROP FUNCTION public.get_workspace_main_id(ws_row workspace);
 
 DROP TABLE public.play_queue_entry;
 
-DROP INDEX idx_type_workspace_unique;
+DROP TRIGGER ensure_main_entry_trigger ON public.play_status;
+DROP FUNCTION public.ensure_main_entry();
 
 DROP TABLE public.play_status;
 
 DROP TABLE public.play_status_type_enum;
 
 DROP TRIGGER reconcile_ordering ON public.file;
-
+DROP FUNCTION public.order_is_reconciled();
 DROP FUNCTION public.reconcile_ordering();
 
 DROP TABLE public.file;
--- hasura/migrations/default/1653299124642_squashed/up.sql
+++ hasura/migrations/default/1653299124642_squashed/up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.workspace
 (
-    id         text        NOT NULL,
+    id         uuid        NOT NULL DEFAULT gen_random_uuid(),
+    name       text        NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
     PRIMARY KEY (id),
@@ -45,14 +46,14 @@ VALUES (E'audioset', E'a set of audio files');
 CREATE TABLE public.file
 (
     id           uuid    NOT NULL DEFAULT gen_random_uuid(),
-    workspace_id text    NOT NULL,
+    workspace_id uuid    NOT NULL,
     download_url text    NOT NULL,
     name         text    NOT NULL,
     description  text    NOT NULL DEFAULT '',
     path         jsonb   NOT NULL DEFAULT jsonb_build_array(),
     type         text    NOT NULL,
     length       numeric NOT NULL,
-    ordering     integer          DEFAULT NULL,
+    ordering     bigint           DEFAULT NULL,
     FOREIGN KEY (workspace_id) REFERENCES public.workspace (id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (type) REFERENCES public.file_type_enum (value) ON UPDATE CASCADE ON DELETE RESTRICT,
     PRIMARY KEY (id),
@@ -63,14 +64,31 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE OR REPLACE FUNCTION public.reconcile_ordering()
     RETURNS TRIGGER AS
 $$
-DECLARE
-    _new record;
 BEGIN
     UPDATE file
     SET ordering = rows.row_number * 100
-    FROM (SELECT id, row_number() over (PARTITION BY workspace_id ORDER BY ordering NULLS LAST) as row_number
+    FROM (SELECT file.id, row_number() over (PARTITION BY workspace_id ORDER BY ordering NULLS LAST) as row_number
           FROM file) as rows
-    WHERE id = rows.id;
+    WHERE file.id = rows.id;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION public.order_is_reconciled()
+    RETURNS bool AS
+$$
+DECLARE
+    _result bool;
+BEGIN
+    SELECT count(*) = 0
+    INTO _result
+    FROM (SELECT file.id,
+                 file.ordering,
+                 (row_number() over (PARTITION BY workspace_id ORDER BY ordering NULLS LAST)) *
+                 100 AS reconciled_ordering
+          FROM file) as order_check
+    WHERE order_check.ordering != order_check.reconciled_ordering;
+    RETURN _result;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -78,9 +96,11 @@ CREATE TRIGGER reconcile_ordering
     AFTER UPDATE OF ordering OR INSERT
     ON public.file
     FOR EACH STATEMENT
+    WHEN (NOT public.order_is_reconciled())
 EXECUTE PROCEDURE public.reconcile_ordering();
 COMMENT ON TRIGGER reconcile_ordering ON public.file
     IS 'trigger to set value of column updated_at to current timestamp on row update';
+
 CREATE TABLE public.play_status_type_enum
 (
     value       text NOT NULL,
@@ -103,11 +123,12 @@ VALUES (E'sfx', E'any ambience');
 CREATE TABLE public.play_status
 (
     id              uuid        NOT NULL DEFAULT gen_random_uuid(),
-    workspace_id    text        NOT NULL,
-    start_timestamp timestamptz NOT NULL,
-    pause_timestamp timestamptz,
-    volume          numeric     NOT NULL,
-    speed           numeric     NOT NULL,
+    workspace_id    uuid        NOT NULL,
+    created_at      timestamptz NOT NULL DEFAULT now(),
+    start_timestamp timestamptz NOT NULL DEFAULT now(),
+    pause_timestamp timestamptz          DEFAULT NULL,
+    volume          numeric     NOT NULL DEFAULT 1,
+    speed           numeric     NOT NULL DEFAULT 1,
     type            text        NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (workspace_id) REFERENCES public.workspace (id) ON UPDATE cascade ON DELETE cascade,
@@ -119,8 +140,21 @@ CREATE TABLE public.play_status
 COMMENT ON TABLE public.play_status IS E'workspace status';
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE UNIQUE INDEX idx_type_workspace_unique ON public.play_status (workspace_id)
-    WHERE type = 'main';
+CREATE OR REPLACE FUNCTION public.ensure_main_entry()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM play_status WHERE type = 'main' AND workspace_id = NEW.workspace_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ensure_main_entry_trigger
+    BEFORE INSERT
+    ON play_status
+    FOR EACH ROW
+    WHEN (NEW.type = 'main')
+EXECUTE FUNCTION public.ensure_main_entry();
 
 CREATE TABLE public.play_queue_entry
 (
@@ -141,6 +175,7 @@ SELECT id
 FROM public.play_status
 WHERE workspace_id = ws_row.id
   AND type = 'main'
+ORDER BY ws_row.created_at DESC
 $$ LANGUAGE sql STABLE;
 
 CREATE TABLE public.job
@@ -155,7 +190,7 @@ CREATE TABLE public.job
     assigned_worker uuid             DEFAULT NULL,
     progress        numeric NOT NULL,
     progress_stage  text    NOT NULL,
-    workspace_id    text    NOT NULL,
+    workspace_id    uuid    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (workspace_id) REFERENCES public.workspace (id) ON UPDATE restrict ON DELETE restrict,
     CONSTRAINT no_empty_jobs CHECK ((file_upload IS NULL) != (url IS NULL))
@@ -167,5 +202,41 @@ CREATE TABLE public.delete_job
     file_id         uuid NOT NULL,
     assigned_worker uuid          DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (file_id) REFERENCES public.file (id) ON UPDATE restrict ON DELETE restrict
+    FOREIGN KEY (file_id) REFERENCES public.file (id) ON UPDATE restrict ON DELETE cascade
 );
+
+CREATE TABLE public.event
+(
+    id           uuid        NOT NULL DEFAULT gen_random_uuid(),
+    time         timestamptz NOT NULL DEFAULT now(),
+    workspace_id uuid        NOT NULL,
+    invalidate   text        NOT NULL,
+    FOREIGN KEY (workspace_id) REFERENCES public.workspace (id) ON UPDATE restrict ON DELETE cascade
+);
+
+CREATE OR REPLACE FUNCTION public.create_event()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    INSERT INTO event (workspace_id, invalidate) VALUES (NEW.workspace_id, tg_argv[0]);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER status_update_event
+    AFTER UPDATE OR INSERT
+    ON public.play_status
+    FOR EACH ROW
+EXECUTE FUNCTION public.create_event('play_status');
+
+CREATE TRIGGER file_update_event
+    AFTER UPDATE OR INSERT
+    ON public.file
+    FOR EACH ROW
+EXECUTE FUNCTION public.create_event('file');
+
+CREATE TRIGGER job_update_event
+    AFTER UPDATE OR INSERT
+    ON public.job
+    FOR EACH ROW
+EXECUTE FUNCTION public.create_event('job');
