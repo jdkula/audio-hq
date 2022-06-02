9f68ce4d942562c79957c176cc9c7e685b373d81
Jonathan Kula <jonathan@jdkula.dev>
2022-06-01 21:43:19 -0700

    Logging and some worker bugfixes

--- worker/src/filesystems/S3FileSystem.ts
+++ worker/src/filesystems/S3FileSystem.ts
@@ -1,79 +1,75 @@
-import FileSystem, { FileInfo } from "./FileSystem";
+import FileSystem from './FileSystem';
 
-import fsProm from "fs/promises";
-import fs from "fs";
+import fsProm from 'fs/promises';
+import fs from 'fs';
 
-import AWS, { Endpoint } from "aws-sdk";
+import AWS, { Endpoint } from 'aws-sdk';
 
 if (process.env.S3_BUCKET_NAME === undefined) {
-  throw new Error("S3_BUCKET_NAME environment variable must be defined!");
+    throw new Error('S3_BUCKET_NAME environment variable must be defined!');
 }
 if (process.env.AWS_ACCESS_KEY_ID === undefined) {
-  throw new Error("AWS_ACCESS_KEY_ID environment variable must be defined!");
+    throw new Error('AWS_ACCESS_KEY_ID environment variable must be defined!');
 }
 if (process.env.AWS_SECRET_ACCESS_KEY === undefined) {
-  throw new Error(
-    "AWS_SECRET_ACCESS_KEY environment variable must be defined!"
-  );
+    throw new Error('AWS_SECRET_ACCESS_KEY environment variable must be defined!');
 }
 
 const defaultParams = {
-  Bucket: process.env.S3_BUCKET_NAME,
+    Bucket: process.env.S3_BUCKET_NAME,
 };
 
 const S3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  ...(process.env.AWS_ENDPOINT_URL
-    ? {
-        endpoint: new Endpoint(process.env.AWS_ENDPOINT_URL),
-        s3ForcePathStyle: true,
-      }
-    : {}),
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    ...(process.env.AWS_ENDPOINT_URL
+        ? {
+              endpoint: new Endpoint(process.env.AWS_ENDPOINT_URL),
+              s3ForcePathStyle: true,
+          }
+        : {}),
 });
 
 export default class S3FileSystem implements FileSystem {
-  async delete(id: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      S3.deleteObject(
-        {
-          ...defaultParams,
-          Key: id,
-        },
-        (err) => {
-          if (err) reject(err);
-          else resolve();
-        }
-      );
-    });
-  }
+    async delete(id: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            S3.deleteObject(
+                {
+                    ...defaultParams,
+                    Key: id,
+                },
+                (err) => {
+                    if (err) reject(err);
+                    else resolve();
+                },
+            );
+        });
+    }
 
-  async write(
-    filepath: string,
-    id: string,
-    contentType: string,
-    onProgress?: ((progress: number | undefined) => void) | undefined
-  ): Promise<string> {
-    const size = (await fsProm.stat(filepath)).size;
+    async write(
+        filepath: string,
+        id: string,
+        contentType: string,
+        onProgress?: ((progress: number | undefined) => void) | undefined,
+    ): Promise<string> {
+        const size = (await fsProm.stat(filepath)).size;
 
-    const upload = new AWS.S3.ManagedUpload({
-      service: S3,
-      params: {
-        ...defaultParams,
-        Key: id,
-        ContentType: contentType,
-        ContentLength: size,
-        Body: fs.createReadStream(filepath),
-        ACL: "public-read",
-      },
-    });
+        const upload = new AWS.S3.ManagedUpload({
+            service: S3,
+            params: {
+                ...defaultParams,
+                Key: id,
+                ContentType: contentType,
+                ContentLength: size,
+                Body: fs.createReadStream(filepath),
+                ACL: 'public-read',
+            },
+        });
 
-    upload.on("httpUploadProgress", (progress) =>
-      onProgress?.(progress.loaded / (progress.total ?? size))
-    );
+        upload.on('httpUploadProgress', (progress) => onProgress?.(progress.loaded / (progress.total ?? size)));
 
-    const data = await upload.promise();
+        const data = await upload.promise();
 
-    return data.Location;
-  }
+        return data.Location;
+    }
 }
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,14 +1,13 @@
-import { createUrqlClient } from "./urql_worker";
-import * as GQL from "./generated/graphql";
-import { pipe, subscribe } from "wonka";
-import { Processor } from "./processor";
-import { v4 } from "uuid";
-import fsProm from "fs/promises";
-import { AppFS } from "./filesystems/FileSystem";
+import { createUrqlClient } from './urql_worker';
+import * as GQL from './generated/graphql';
+import { pipe, subscribe } from 'wonka';
+import { Processor } from './processor';
+import { v4 } from 'uuid';
+import { AppFS } from './filesystems/FileSystem';
 
-import { Logger } from "tslog";
+import { Logger } from 'tslog';
 
-const log = new Logger({ name: "worker" });
+const log = new Logger({ name: 'worker' });
 
 const client = createUrqlClient();
 const processor = new Processor(client);
@@ -17,133 +16,124 @@ const myid = v4();
 let working = false;
 
 async function doJobs() {
-  if (working) return;
-  working = true;
-  try {
-    while (true) {
-      log.silly("Attempting to claim a job...");
-      const jobRaw = await client
-        .mutation<GQL.ClaimJobMutation, GQL.ClaimJobMutationVariables>(
-          GQL.ClaimJobDocument,
-          {
-            myId: myid,
-          }
-        )
-        .toPromise();
+    if (working) return;
+    working = true;
+    try {
+        while (true) {
+            log.silly('Attempting to claim a job...');
+            const jobRaw = await client
+                .mutation<GQL.ClaimJobMutation, GQL.ClaimJobMutationVariables>(GQL.ClaimJobDocument, {
+                    myId: myid,
+                })
+                .toPromise();
 
-      const job = jobRaw.data?.claim_job;
-      if (!job || !job.id) {
-        log.silly("No job found. Waiting for more jobs...");
-        return;
-      }
+            const job = jobRaw.data?.claim_job;
+            if (!job || !job.id) {
+                log.silly('No job found. Waiting for more jobs...');
+                return;
+            }
 
-      log.debug("Job found", job);
-      let inPath;
-      if (job.file_upload) {
-        log.silly("Raw file upload found. Saving to disk...");
-        inPath = await processor.saveInput(job.file_upload);
-      } else if (job.url) {
-        log.silly("Import found. Downloading...");
-        inPath = await processor.download(job.url, job.id, job.options);
-      } else {
-        throw new Error("Neither job nor file upload is defined!");
-      }
+            log.debug('Job found', job);
+            let inPath;
+            if (job.file_upload) {
+                log.silly('Raw file upload found. Saving to disk...');
+                inPath = await processor.saveInput(job.file_upload);
+            } else if (job.url) {
+                log.silly('Import found. Downloading...');
+                inPath = await processor.download(job.url, job.id, job.options);
+            } else {
+                throw new Error('Neither job nor file upload is defined!');
+            }
 
-      log.silly("Converting...");
-      const outPath = await processor.convert(inPath, job.id, job.options);
+            log.silly('Converting...');
+            const outPath = await processor.convert(inPath, job.id, job.options);
 
-      log.silly("Adding to AHQ...");
-      await processor.addFile(job.id, outPath, {
-        name: job.name,
-        path: job.path,
-        workspace: job.workspace_id,
-        description: job.description,
-      });
+            log.silly('Adding to AHQ...');
+            await processor.addFile(job.id, outPath, {
+                name: job.name,
+                path: job.path,
+                workspace: job.workspace_id,
+                description: job.description,
+            });
 
-      log.debug("Job finished");
+            log.debug('Job finished');
+        }
+    } finally {
+        working = false;
     }
-  } finally {
-    working = false;
-  }
 }
 
 let deleting = false;
 
 async function dpDeletion() {
-  if (deleting) return;
-  deleting = true;
-  try {
-    while (true) {
-      log.silly("Attempting to claim a delete job...");
-      const jobRaw = await client
-        .mutation<
-          GQL.ClaimDeleteJobMutation,
-          GQL.ClaimDeleteJobMutationVariables
-        >(GQL.ClaimDeleteJobDocument, {
-          myId: myid,
-        })
-        .toPromise();
+    if (deleting) return;
+    deleting = true;
+    try {
+        while (true) {
+            log.silly('Attempting to claim a delete job...');
+            const jobRaw = await client
+                .mutation<GQL.ClaimDeleteJobMutation, GQL.ClaimDeleteJobMutationVariables>(GQL.ClaimDeleteJobDocument, {
+                    myId: myid,
+                })
+                .toPromise();
 
-      const job = jobRaw.data?.claim_delete_job;
-      if (!job || !job.id) {
-        log.silly("No delete job found. Waiting for more delete jobs...", job);
-        return;
-      }
+            const job = jobRaw.data?.claim_delete_job;
+            if (!job || !job.id) {
+                log.silly('No delete job found. Waiting for more delete jobs...', job);
+                return;
+            }
 
-      log.debug("Delete job received", job);
-      log.silly("Deleting file in database...");
-      await client
-        .mutation<
-          GQL.CommitDeleteJobMutation,
-          GQL.CommitDeleteJobMutationVariables
-        >(GQL.CommitDeleteJobDocument, {
-          jobId: job.id,
-          fileId: job.file.id,
-        })
-        .toPromise();
+            log.debug('Delete job received', job);
+            log.silly('Deleting file in database...');
+            await client
+                .mutation<GQL.CommitDeleteJobMutation, GQL.CommitDeleteJobMutationVariables>(
+                    GQL.CommitDeleteJobDocument,
+                    {
+                        jobId: job.id,
+                        fileId: job.file.id,
+                    },
+                )
+                .toPromise();
 
-      if (job.file.provider_id) {
-        log.silly("Deleting in AWS...");
-        await AppFS.delete(job.file.provider_id);
-      } else {
-        log.debug(
-          "No provide ID associated with this file, skipping AWS delete..."
-        );
-      }
+            if (job.file.provider_id) {
+                log.silly('Deleting in AWS...');
+                await AppFS.delete(job.file.provider_id);
+            } else {
+                log.debug('No provide ID associated with this file, skipping AWS delete...');
+            }
 
-      log.debug("Delete job finished");
+            log.debug('Delete job finished');
+        }
+    } finally {
+        deleting = false;
     }
-  } finally {
-    deleting = false;
-  }
 }
 
-const newJobs = client.subscription<
-  GQL.NewJobsSubscriptionSubscription,
-  GQL.NewJobsSubscriptionSubscriptionVariables
->(GQL.NewJobsSubscriptionDocument);
+const newJobs = client.subscription<GQL.NewJobsSubscriptionSubscription, GQL.NewJobsSubscriptionSubscriptionVariables>(
+    GQL.NewJobsSubscriptionDocument,
+);
 
 const newDeleteJobs = client.subscription<
-  GQL.DeleteJobsSubscriptionSubscription,
-  GQL.DeleteJobsSubscriptionSubscriptionVariables
+    GQL.DeleteJobsSubscriptionSubscription,
+    GQL.DeleteJobsSubscriptionSubscriptionVariables
 >(GQL.DeleteJobsSubscriptionDocument);
 
 pipe(
-  newJobs,
-  subscribe((result) => {
-    log.silly("Got new jobs data", result.data);
-    if (!result.data || result.data.job.length == 0) return;
-    doJobs();
-  })
+    newJobs,
+    subscribe((result) => {
+        log.silly('Got new jobs data', result.data);
+        if (!result.data || result.data.job.length == 0) return;
+        doJobs();
+    }),
 );
 
 pipe(
-  newDeleteJobs,
-  subscribe((result) => {
-    log.silly("Got new delete jobs data", result.data);
-    if (!result.data || result.data.delete_job.length == 0) return;
-    dpDeletion();
-  })
+    newDeleteJobs,
+    subscribe((result) => {
+        log.silly('Got new delete jobs data', result.data);
+        if (!result.data || result.data.delete_job.length == 0) return;
+        dpDeletion();
+    }),
 );
 
-log.info("Worker started");
+log.info('Worker started');
