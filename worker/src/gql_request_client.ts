f62727eb8162945d4acb4965a5581a368f109c39
Jonathan Kula <jonathan@jdkula.dev>
2022-11-12 03:17:23 -0800

    Make sure it uses the HTTP URL

--- worker/src/gql_request_client.ts
+++ worker/src/gql_request_client.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
 export function createGraphqlRequestClient(): GraphQLClient {
-    return new GraphQLClient(process.env.NEXT_PUBLIC_HASURA_URL_WS as string, {
+    return new GraphQLClient(process.env.NEXT_PUBLIC_HASURA_URL_HTTP as string, {
         headers: {
             'X-Hasura-Admin-Secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET as string,
             'X-Hasura-Role': 'worker',
